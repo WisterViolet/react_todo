a319339e9ea5bad492c5cd5d420eb224f0921c82
WisterViolet <fujianifan@gmail.com>
2025-01-17 00:58:22 +0900

    Create Application Mock

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,59 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type task = {
+  Name:string,
+  Limit:number,
+  Finished:boolean
+}
+
+const TASKS = [
+  {Name:"Homework", Limit:1, Finished:false},
+  {Name:"Desk", Limit:15, Finished:true},
+  {Name:"Travel", Limit:5, Finished:false},
+  {Name:"Game", Limit:100, Finished:true},
+  {Name:"Idolm@ater Millionlive!", Limit:1000000, Finished:false},
+];
+
+
+function SearchBar(){
+  return (
+    <form>
+      <input type="text" placeholder='Search' />
+      <label>
+      <input type="checkbox" />Only show tasks unfinished
+      </label>
+    </form>
+  );
+}
+
+function TodoTable({tasks}:{tasks:Array<task>}){
+  const taskList = tasks.map((task, index) => {
+    const fnd = task.Finished ? "Yes" : "No";
+    return <p key={index}>{task.Name} {task.Limit+" Days"} {fnd}</p>;
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p> Name Limit Finished</p>
+      {taskList}
+    </div>
   )
 }
+
+function TodoApp({tasks}:{tasks:Array<task>}){
+  return (
+    <div>
+      <SearchBar />
+      <TodoTable tasks={tasks} />
+    </div>
+  );
+}
+
+function App() {
+  return <TodoApp tasks={TASKS}/>;
+}
+
+
 
 export default App
